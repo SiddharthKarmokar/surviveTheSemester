52bd7f13f089a8d830241d1731c9f835d9373c83
SiddharthKarmokar <siddkarmokar@gmail.com>
2026-03-06 22:26:50 +0530

    vibe maxing 1

--- apps/backend/utils/send-mail/index.js
+++ apps/backend/utils/send-mail/index.js
@@ -8,6 +8,7 @@ const transporter = nodemailer.createTransport({
     host:process.env.SMTP_HOST,
     port: Number(process.env.SMTP_PORT) || 587,
     secure: false,
+    family: 4,
     service: process.env.SMTP_SERVICE,
     auth:{
         user:process.env.SMTP_USER,
