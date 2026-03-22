fdd74da6d4fee778f222fa2d06d67c51a2cb955c
AnilKumt <anilkumt51@gmail.com>
2026-03-22 18:20:19 +0530

    removed top users row

--- apps/Frontend/src/pages/Dashboard/Dashboard.jsx
+++ apps/Frontend/src/pages/Dashboard/Dashboard.jsx
@@ -90,7 +90,7 @@ const Dashboard = () => {
         }
         return (
             <>
-                <TopUserRow />
+                {/* <TopUserRow /> */}
                 <GameCardGrid onPlayGame={handlePlayGame} />
             </>
         );
